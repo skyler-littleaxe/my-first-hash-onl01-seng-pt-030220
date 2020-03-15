2a3960c64e8e2a0428adf23c7d9a5c237a992884
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-15 00:16:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,13 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"Day1" =>  "Leg Day", "Day2" => "Chest/Tri/Shoulder Day", "Day3" => "Back/Biceps"}
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  the_manifest = {"Corsets" => ["corset1", "corset2", "corset3", "corset4", "corset5"] "Vases" =>  }
 
 
 end
