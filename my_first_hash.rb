b48f58f5031960eb5b6fbbdfa23ba0872979d602
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-15 00:21:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  the_manifest = {"whale bone corsets" => ["corset1", "corset2", "corset3", "corset4", "corset5"], "porcelain vases" => ["vase1", "vase2"], "oil paintings" => ["painting1", "painting2", "painting3"] }
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
 end
 
 
